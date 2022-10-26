bf21348b75baf97baf78826f12d2c51bd8fd21db
parkeradams <parkeradams1@mail.weber.edu>
2022-10-25 20:23:03 -0600

    implemented CRUD for student projects

--- Server_Manager/project/views.py
+++ Server_Manager/project/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Project
+from .forms import ProjectForm
+
+
+def createProject(request):
+    form = ProjectForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('studentHome')
+
+    return render(request, 'project/createProject.html', {'form': form})
 
 # Create your views here.
+
+
+def projectDetail(request, id):
+    project = Project.objects.get(pk=id)
+    context = {
+        'project': project
+    }
+    return render(request, 'project/projectDetail.html', context)
+
+
+def updateProject(request, id):
+    project = Project.objects.get(pk=id)
+    form = ProjectForm(request.POST or None, instance=project)
+    if form.is_valid():
+        form.save()
+        return redirect('studentHome')
+    return render(request, 'project/editProject.html', {'form': form, 'project':project})
+
+
+def deleteProject(request, id):
+    project = Project.objects.get(pk=id)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('studentHome')
+    return render(request, 'project/deleteProject.html', {'project':project})
