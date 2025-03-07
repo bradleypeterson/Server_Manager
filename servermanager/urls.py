8ea85355addef2b214eb21da79aa934b04af7a0b
rivertolsonWSU <rivertolson@mail.weber.edu>
2025-03-07 14:51:09 -0700

    Started password reset functionality

--- servermanager/urls.py
+++ servermanager/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', user_views.home, name="home"),
     path('register/', user_views.register, name="register"),
-    path('resetPassword/', user_views.password_reset, name="resetPassword"),
+    path('resetPassword/<int:user_id>/', user_views.password_reset, name="resetPassword"),
     path('addProject/', user_views.addProject, name="addProject"),
     path('', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="logout.html"), name='logout'),
--- user/views.py
+++ user/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import LoginForm, RegistrationForm, ResetPasswordForm
 from django.contrib import messages
+
+from .models import AppUser
+
 
 # Create your views here.
 def register(request):
@@ -51,9 +55,23 @@ def login_user(request):
 
     return render(request, 'login.html', {'form': form})
 
-def password_reset(request):
-    form = ResetPasswordForm(request.POST)
-    return render(request, "resetPassword.html", {'form': form})
+def password_reset(request, user_id):
+    if request.method == 'POST':
+        form = ResetPasswordForm(request.POST)
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.error(request, 'Passwords do not match')
+        else:
+            user = get_object_or_404(AppUser, id=user_id)
+            user.set_password(password1)
+            user.save()
+            messages.success(request, 'Password Reset Successful')
+            return redirect('/')
+    else:
+        form = ResetPasswordForm(request.POST)
+    return render(request, "resetPassword.html", {'form': form, 'user_id': user_id})
 
 @login_required
 def home(request):
