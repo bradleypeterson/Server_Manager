e1974764629e77b3aefd06b2a33809bec4ca0e53
cameronmckay <cameronmckay62@gmail.com>
2025-03-19 17:16:49 -0600

    Config stuff

--- servermanager/settings.py
+++ servermanager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1npil10ruxkk+37my*zl9kld#m_mqyg^&==zkmo5#r(!t5rd-g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'testserver']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'cameronmckay.xyz']
 
 
 # Application definition
