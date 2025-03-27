0ef6806618c35a88f2ea0ff44b105edde99d7ea2
Aditya Paralikar <adityaparalikar@mail.weber.edu>
2025-03-27 17:16:16 -0600

    Updated group front end

--- group/forms.py
+++ group/forms.py
@@ -3,27 +3,19 @@ from .models import Group
 from user.models import AppUser
 from project.models import Project
 
+
 class GroupForm(forms.ModelForm):
-    group_name = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'Group Name', 'style': 'width: 300px;', 'class': 'form-control'})
-    )
-
-    description = forms.CharField(
-        widget=forms.Textarea(attrs={'placeholder': 'Enter group description', 'rows': 3, 'class': 'form-control'})
-    )
-
-    users = forms.ModelMultipleChoiceField(
-        queryset=AppUser.objects.all(),
-        widget=forms.CheckboxSelectMultiple(),
-        required=False
-    )
-
-    projects = forms.ModelMultipleChoiceField(
-        queryset=Project.objects.all(),
-        widget=forms.CheckboxSelectMultiple(),
-        required=False
-    )
-
     class Meta:
         model = Group
         fields = ['group_name', 'description', 'users', 'projects']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['group_name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Group Name'})
+        self.fields['description'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Enter group description', 'rows': 3})
+        self.fields['users'].widget.attrs.update({'class': 'form-check-input'})
+        self.fields['projects'].widget.attrs.update({'class': 'form-check-input'})
+        self.fields['users'].widget.attrs.update({'class': 'form-select'})
+        self.fields['projects'].widget.attrs.update({'class': 'form-select'})
