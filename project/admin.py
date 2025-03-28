ead3b707797a7fb3aeb498cdedcc99565bf904e6
cameron-mckay <cameronmckay62@gmail.com>
2025-03-28 11:34:25 -0600

    Forgot a dot

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Project, OperatingSystem, Server
+from .models import Project, OperatingSystem, Server
 
 admin.site.register(Project)
 admin.site.register(OperatingSystem)
