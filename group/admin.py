49b33aac46a04d960d363fa0a167b5d39743f39e
cameron-mckay <cameronmckay62@gmail.com>
2025-03-28 11:33:31 -0600

    Updated admin page

--- group/admin.py
+++ group/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Group
+from django.contrib.auth.models import Group as DjangoGroup
 
 # Register your models here.
 
 admin.site.register(Group)
+admin.site.unregister(DjangoGroup)
--- project/admin.py
+++ project/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from models import Project, OperatingSystem, Server
 
-admin.site.unregister(Group)
+admin.site.register(Project)
+admin.site.register(OperatingSystem)
+admin.site.register(Server)
